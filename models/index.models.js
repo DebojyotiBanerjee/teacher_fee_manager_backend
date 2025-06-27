a909f5709bc6f0931b9bd62031d108d87e4e89d5
Debojyoti Banerjee <debojyotibanerjee2002@gmail.com>
2025-06-27 18:41:47 +0530

    made models of  batches,rating,subjet,detail of student and teacher, updated index

--- models/index.models.js
+++ models/index.models.js
@@ -1,5 +1,13 @@
 const user = require('./user.models');
+const batch = require('./batch.models');
+const teacher = require('./teacher.models');
+const subject = require('./subject.models');
+const rating = require('./rating.models');
 
 module.exports = {
   user,
+  batch,
+  teacher,
+  subject,
+  rating
 };
