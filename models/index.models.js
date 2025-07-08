80e347ea67ad3fb7fbc75cc9bdb4d3d6f240d83f
kochi0903 <kroy8900391997@gmail.com>
2025-07-08 22:31:33 +0530

    fix: correct model imports and typos; add batch schema definition; update rating schema to remove subject reference

--- models/index.models.js
+++ models/index.models.js
@@ -1,11 +1,11 @@
 const user = require('./user.models.js');
-const batchSchema = require('./batch.moddels.js');
-const teacher = require('./detailTeacher.models.js');
+const batchSchema = require('./batch.models.js');
+const detailTeacher = require('./detailTeacher.models.js');
 const rating = require('./rating.models.js');
 
 module.exports = {
   user,
   batchSchema,
-  teacher,  
+  detailTeacher,  
   rating
 };
